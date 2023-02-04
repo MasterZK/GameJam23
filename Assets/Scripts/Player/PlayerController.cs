8d21f516ecdd8a909f1334f0ba837000ea6b8f45
LukasLeuthold <Leuthold.Lukas@web.de>
2023-02-04 21:36:41 +0100

    Player + teleporter

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,62 +6,91 @@ public class PlayerController : MonoBehaviour
 {
     public float PlayerMovespeed;
     public float PlayerJumpForce;
-    
+
 
     [SerializeField] private LayerMask groundLayer;
     [SerializeField] private GameObject groundCheck;
 
     private bool isGrounded;
+    private bool canMove = true;
     private string currentSate;
 
     private Rigidbody2D rb;
     private Animator anim;
     private SpriteRenderer spriteRenderer;
 
-    private Vector2 moveImput;
+    private Vector2 moveInput;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        anim= GetComponent<Animator>();
-        spriteRenderer= GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
     private void Update()
     {
-        transform.Translate(new Vector3(moveImput.x,0,0) * PlayerMovespeed * Time.deltaTime);
         isGrounded = Physics2D.OverlapCircle(groundCheck.transform.position, 0.3f, groundLayer);
+        if (canMove)
+        {
+            transform.Translate(new Vector3(moveInput.x, 0, 0) * PlayerMovespeed * Time.deltaTime);
 
-        if (moveImput.x > 0.2)
-        {
-            ChangeAnimationState("PlayerWalk");
-            spriteRenderer.flipX = false;
+            if (isGrounded)
+            {
+
+                if (moveInput.x > 0.2)
+                {
+                    spriteRenderer.flipX = false;
+                    ChangeAnimationState("PlayerWalk");
+
+                }
+                else if (moveInput.x < -0.2f)
+                {
+                    spriteRenderer.flipX = true;
+                    ChangeAnimationState("PlayerWalk");
+
+                }
+                else
+                {
+                    ChangeAnimationState("PlayerIdle");
+                    Debug.Log("Idle");
+                }
+            }
         }
-        else if (moveImput.x < -0.2f)
+
+    }
+
+    public void OnItemSwitch(InputAction.CallbackContext ctx)
+    {
+
+
+    }
+    public void OnPlayerIteract(InputAction.CallbackContext ctx)
+    {
+        float time = 1;
+        string animation = "";
+        if (false)
         {
-            ChangeAnimationState("PlayerWalk");
-            spriteRenderer.flipX= true;
+            StartCoroutine(PlayerInteraction(time, animation));
         }
         else
         {
-            ChangeAnimationState("PlayerIdle");
+            ChangeAnimationState("Attack");
         }
-    }
 
+    }
     public void OnMove(InputAction.CallbackContext ctx)
     {
-        moveImput = ctx.ReadValue<Vector2>();
-        
+        moveInput = ctx.ReadValue<Vector2>();
     }
     public void OnJump(InputAction.CallbackContext ctx)
     {
 
-        if (ctx.performed && isGrounded)
+        if (ctx.performed && isGrounded && canMove)
         {
-            rb.AddForce(Vector3.up * PlayerJumpForce, ForceMode2D.Impulse);
             anim.PlayInFixedTime("PlayerJump");
+            rb.AddForce(Vector3.up * PlayerJumpForce, ForceMode2D.Impulse);
         }
     }
-
     private void ChangeAnimationState(string newState)
     {
         if (currentSate == newState) return;
@@ -70,7 +98,19 @@ public class PlayerController : MonoBehaviour
         anim.CrossFade(newState, 0.1f);
 
         currentSate = newState;
-  
+
+    }
+    public void ToggleMovement()
+    {
+        canMove = !canMove;
+    }
+    IEnumerator PlayerInteraction(float time, string animation)
+    {
+        canMove = false;
+        ChangeAnimationState(animation);
+        yield return new WaitForSeconds(time);
+
+        canMove = true;
     }
 
 }
